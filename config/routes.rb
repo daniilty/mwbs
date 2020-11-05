391fb30a74474de8d876ca57f3522460e8442e8b
Daniel <adef96@mail.ru>
2020-11-05 03:41:47 +0300

    added second project

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'contacts', to: 'home#contacts'
   get 'projects', to: 'home#projects'
   get 'dezprof', to: 'home#first'
-  get 'home/second'
+  get 'second', to: 'home#second'
   get 'home/third'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
